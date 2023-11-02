c158055bd216fe29fa970efb44e8a148acd0dbbd
Enimalojd <enderston8@gmail.com>
2023-11-02 19:04:34 +0400

    fix

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,6 @@ import prompt
 
 
 def get_username():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    return {name}
