0fe16906e77b11ede4167ad3e2a2fb6dcb65f1dd
Enimalojd <enderston8@gmail.com>
2023-11-03 17:30:40 +0400

    add gcd game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+
+def get_description():
+    return 'Find the greatest common divisor of given numbers.\n'
+
+
+def get_question_answer():
+    dig1 = random.randint(1, 50)
+    dig2 = random.randint(1, 50)
+
+    question = f'{dig1} {dig2}'
+    answer = math.gcd(dig1, dig2)
+
+    return question, answer
