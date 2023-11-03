9d2caea5271cad000862b46066dd44b4b4c9381d
Enimalojd <enderston8@gmail.com>
2023-11-03 16:58:50 +0400

    fix

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -9,10 +9,10 @@ def get_question_answer():
     dig1 = random.randint(0, 50)
     dig2 = random.randint(0, 50)
 
-    operators = ['+', '-', '*', '/']
+    operators = ['+', '-', '*',]
     operator = random.choice(operators)
 
-    question = f'{dig1}{operator}{dig2}'
+    question = f'{dig1} {operator} {dig2}'
     answer = eval(question)
 
-    return question, answer
+    return question, str(answer)
