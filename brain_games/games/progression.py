2da02238cbc3ace123a5ee890511c9198bfce55b
Enimalojd <enderston8@gmail.com>
2023-11-03 20:39:52 +0400

    add logic for game progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,6 +1,21 @@
+import random
+
+
 def get_description():
     return 'What number is missing in the progression?\n'
 
 
 def get_question_answer():
-    pass
+    start = random.randint(1, 5)
+    end = random.randint(30, 33)
+    step = random.randint(2, 4)
+
+    progression = list(range(start, end+1, step))
+    random_index = random.randint(1, len(progression)-1)
+
+    answer = progression[random_index]
+    progression[random_index] = '...'
+    question = ' '.join(map(str, progression))
+
+    return question, str(answer)
+
