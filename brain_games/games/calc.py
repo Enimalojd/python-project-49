29ce1b52ecaadccb411ee338b7886e5ab636db1c
Enimalojd <enderston8@gmail.com>
2023-11-03 16:38:41 +0400

    logic calc game

---
+++ brain_games/games/calc.py
@@ -0,0 +1,18 @@
+import random
+
+
+def get_description():
+    return 'What is the result of the expression?'
+
+
+def get_question_answer():
+    dig1 = random.randint(0, 50)
+    dig2 = random.randint(0, 50)
+
+    operators = ['+', '-', '*', '/']
+    operator = random.choice(operators)
+
+    question = f'{dig1}{operator}{dig2}'
+    answer = eval(question)
+
+    return question, answer
