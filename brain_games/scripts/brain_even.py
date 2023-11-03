99f2adc7831660763f86304f308004b96cf0b962
Enimalojd <enderston8@gmail.com>
2023-11-03 16:09:40 +0400

    Вынес всю общую информацию/логику для будущих игр в отдельный файл src.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.even import even_game
+from brain_games.src import run_game
+from brain_games.games import even
 
 
 def main():
-    even_game()
+    run_game(even)
 
 
 if __name__ == '__main__':
---
+++ brain_games/src.py
@@ -0,0 +1,27 @@
+import prompt
+
+
+def run_game(game):
+    print('Welcome to the Brain Games!')
+
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print(game.get_description())
+
+    game_rounds = 3
+
+    while game_rounds >= 0:
+
+        question, answer = game.get_question_answer()
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer == answer:
+            print('Correct!')
+            game_rounds -= 1
+        else:
+            print(f"{user_answer} is wrong answer ;(. Correct answer was {answer} Let's try again, {name}!")
+            break
+
+    if game_rounds == 0:
+        print(f'Congratulations, {name}!')
