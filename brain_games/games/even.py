794a017344e0aebb56e13165ce04aeb8d38c5145
enimalojd <enderston8@gmail.com>
2023-11-04 22:38:18 +0400

    fix syntax

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 def get_description():
-    return "Answer 'yes' if the number is even, otherwise answer 'no'.\n"
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_question_answer():
--- brain_games/src.py
+++ brain_games/src.py
@@ -17,7 +17,7 @@ def run_game(game):
         user_answer = prompt.string('Your answer: ')
 
         if user_answer == answer:
-            print('Correct!\n')
+            print('Correct!')
             game_rounds -= 1
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
