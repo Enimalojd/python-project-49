b687cf3af00778a47e4807d1f9b3753f6812ceae
enimalojd <enderston8@gmail.com>
2023-11-08 20:03:45 +0400

    refactoring

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,19 +10,15 @@ def run_game(game):
 
     game_rounds = 3
 
-    while game_rounds > 0:
-
+    for round_number in range(0, game_rounds):
         question, answer = game.get_question_answer()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
 
-        if user_answer == answer:
-            print('Correct!')
-            game_rounds -= 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{answer}'. Let's try again, {name}!")
-            break
+        if answer != user_answer:
+            print(f"'{user_answer}' is wrong answer ;(."
+                  f" Correct answer was '{answer}'. Let's try again, {name}!")
+            return
+        print('Correct!')
 
-    if game_rounds == 0:
-        print(f'Congratulations, {name}!')
+    print(f'Congratulations, {name}!')
